3401ecc1f87b65dbdbd5473959e3d04d0305c2c8
Nikita <passpuffnikita@gmail.com>
2024-11-24 19:05:41 +0300

    add 404 in page Reviews

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,15 @@
- const ReviewId = async ({params}: {params: {
+import { notFound } from "next/navigation";
+
+const ReviewId = async ({params}: {params: {
     reviewId: string;
     productId: string;
   }}) => {
   const {reviewId} = await params;
   const {productId} = await params
 
+  if(parseInt(params.reviewId) >= 1000) {
+    notFound()
+  }
   return (
     <div>
       <h1>Review {reviewId} for product {productId}</h1>
